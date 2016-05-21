c2ddec678d8b73f2c9c2bf91bdcab3d797ce3d09
xujiadong <xujd@billbear.cn>
2016-05-21 14:18:13 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <pager :now-page="nowPage" :page-count="20" :padding="3"></pager>
+  </div>
+</template>
+
+<script>
+import Pager from './components/Pager'
+export default {
+  components: {
+    Pager
+  },
+  data () {
+    return {
+      nowPage: 1
+    }
+  },
+  events: {
+    'page-jump' (val) {
+      this.nowPage = val
+    }
+  }
+}
+</script>
